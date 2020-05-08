f0d4161a92ca91d6eaef6dac25bb2d68ecddbfc9
Irfan Ezani <irfanezani@gmail.com>
2020-05-09 02:55:59 +0800

    set new animation

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const circles = document.querySelectorAll('.circle')
+const circle = document.getElementsByClassName('circle')
+const red = document.getElementsByClassName('red')
+const blue = document.getElementsByClassName('blue')
+const yellow = document.getElementsByClassName('yellow')
+const green = document.getElementsByClassName('green')
+const button = document.getElementById('btn')
 
-setInterval(() => {
-  changeLight();
-},500);
-
-function changeLight() {
-circles.classList.remove()
-}
+button.addEventListener('click', function() {
+ circle.classList.remove('red');
+ circle.classList.remove('blue');
+ circle.classList.remove('yellow');
+ circle.classList.remove('green');
+})
